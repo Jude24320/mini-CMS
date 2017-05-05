226b38babd5080c055ddc3bcf0bb2a964ae7fb64
Jude <jude.mbengani@hotmail.fr>
2017-05-05 13:49:24 +0200

    mise a jour database

--- database/database.php
+++ database/database.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-        $bdd = new PDO('mysql:host=localhost;dbname=mini-cms;charset=utf8', 'phpmyadmin', 'paris18');
+        $bdd = new PDO('mysql:host=localhost;dbname=mini-cms;charset=utf8', 'root', 'carton');
       } catch (Exception $e) {
         die('Erreur :'.$e->getMessage());
       }
